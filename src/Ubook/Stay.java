639d77349252b7ec5a36ceaac5ba7f04ffac3917
Aaron <amcghie54@gmail.com>
2017-03-21 08:43:10 -0600

    bulk stays finished

--- src/Ubook/Stay.java
+++ src/Ubook/Stay.java
@@ -1,16 +1,25 @@
 package Ubook;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.*;
 
 public class Stay {
-	public void recordStay(String userName, Statement stmt) {
+	public void showReservations(String userName, Statement stmt, List<String> pids) {
 		try {
-			String sql = "SELECT R.pid, R.cost, H.hid, H.name, P.fromDate, P.toDate,  "
+			String sql = "SELECT R.pid, R.cost, H.hid, H.name, P.fromDate, P.toDate  "
 					+ "FROM Reserves R, TH H, Period P "
-					+ "WHERE P.pid = R.pid AND R.hid = H.hid AND login = '" + userName + "';";
+					+ "WHERE P.pid = R.pid AND R.hid = H.hid AND R.login = '" + userName + "'"
+					+ "AND R.pid NOT IN (SELECT pid FROM Visits)"
+					+ "AND R.pid NOT IN ('', ";
+			String pidsString = "";
+			for(String pid: pids) {
+				pidsString += pid + ", ";
+			}
+			sql += pidsString;
+			sql = sql.substring(0, sql.length() - 2);
+			sql += ");";
 			ResultSet rs = stmt.executeQuery(sql);
 			while(rs.next()) {
 				System.out.println("Id: " + rs.getString("R.pid")
@@ -20,11 +29,17 @@ public class Stay {
 				+ ", Cost: " + rs.getString("cost"));
 				
 			}
-			System.out.println("Enter the Id of a reservation to record your stay:");
-			String pid = MainMenu.input.readLine();
+		}
+		catch (SQLException e) {
 			
-			sql = "INSERT INTO Visits (pid, hid, login, cost)"
-					+ " SELECT Reserves pid, hid, login, cost"
+		} 
+	}
+	
+	public void recordStay(String userName, Statement stmt, String pid) {
+		try {
+			String sql = "INSERT INTO Visits (pid, hid, login, cost)"
+					+ " SELECT pid, hid, login, cost"
+					+ " FROM Reserves"
 					+ " WHERE pid = " + pid + ";";
 			stmt.executeUpdate(sql);
 			
@@ -32,9 +47,32 @@ public class Stay {
 		catch (SQLException e) {
 			
 		} 
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+	
+	public void printSelected(String userName, Statement stmt, List<String> pids) {
+		try {
+			String sql = "SELECT R.pid, R.cost, H.hid, H.name, P.fromDate, P.toDate  "
+					+ "FROM Reserves R, TH H, Period P "
+					+ "WHERE P.pid = R.pid AND R.hid = H.hid AND R.login = '" + userName + "'"
+					+ "AND R.pid IN ('', ";
+			String pidsString = "";
+			for(String pid: pids) {
+				pidsString += pid + ", ";
+			}
+			sql += pidsString;
+			sql = sql.substring(0, sql.length() - 2);
+			sql += ");";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				System.out.println("Id: " + rs.getString("R.pid")
+				+ ", Name: " + rs.getString("H.name")
+				+ ", From: "+ rs.getString("P.fromDate")
+				+ ", To: " + rs.getString("toDate")
+				+ ", Cost: " + rs.getString("cost"));
+			}
 		}
+		catch (SQLException e) {
+			
+		} 
 	}
 }
